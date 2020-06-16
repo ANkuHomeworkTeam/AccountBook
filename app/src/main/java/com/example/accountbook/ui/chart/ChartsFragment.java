fd7d8d872976c6e4cba7d986105942079eb5c197
hrl <993855014@qq.com>
2020-06-16 14:38:55 +0800

    modify some code

---
+++ app/src/main/java/com/example/accountbook/ui/chart/ChartsFragment.java
@@ -0,0 +1,28 @@
+package com.example.accountbook.ui.chart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.accountbook.R;
+
+public class ChartsFragment extends Fragment {
+
+    private ChartsViewModel chartsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        chartsViewModel =
+                ViewModelProviders.of(this).get(ChartsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_charts, container, false);
+        return root;
+    }
+}
