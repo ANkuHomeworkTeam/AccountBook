c82a072716a6b08df2a76c127709bdf326bb1dbf
hrl <993855014@qq.com>
2020-06-16 21:54:23 +0800

    chart

--- app/src/main/java/com/example/accountbook/ui/chart/ChartsFragment.java
+++ app/src/main/java/com/example/accountbook/ui/chart/ChartsFragment.java
@@ -1,30 +1,81 @@
 package com.example.accountbook.ui.chart;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.accountbook.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.model.ValueShape;
 import lecho.lib.hellocharts.view.LineChartView;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class ChartsFragment extends Fragment {
 
-    private ChartsViewModel chartsViewModel;
     private LineChartView lineChartView;
+    private PieChartView pieChartViewIncome;
+    private PieChartView pieChartViewExpense;
+    private LineChartData lineChartData;
+    private Line incomeLine;
+    private Line expenseLine;
+    private Axis axisX;
+    private Axis axisY;
+    private PieChartData pieChartIncomeData;
+    private PieChartData pieChartExpenseData;
+
+    static final private int LINE_CHART_EXPENSE_COLOR = 0xffff6666;
+    static final private int LINE_CHART_INCOME_COLOR = 0xff99cc99;
+
+    static final private ValueShape LINE_CHART_INCOME_DOT = ValueShape.DIAMOND;
+    static final private ValueShape LINE_CHART_EXPENSE_DOT = ValueShape.SQUARE;
+
+    static final private int[] pieChartLabelColor = {
+            0xff996699,
+            0xff666666,
+            0xff003366,
+            0xffffff99,
+            0xff99cc99,
+            0xff6666cc
+    };
+
+
+    static final private int CHART_ON_UNDEFINED = 0;
+    static final private int CHART_ON_MONTH = 1;
+    static final private int CHART_ON_SEASON = 2;
+    static final private int CHART_ON_YEAR = 3;
+    static final private int statusActiveColor = 0xFF00574B;
+    static final private int statusInactiveColor = Color.BLACK;
+    private int chartStatus;
+
+    private Button buttonSwitchToMonth;
+    private Button buttonSwitchToSeason;
+    private Button buttonSwitchToYear;
+
+    private TextView incomeSumDisplay;
+    private TextView expenseSumDisplay;
+
+    private double income;
+    private double expense;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        chartsViewModel =
-                ViewModelProviders.of(this).get(ChartsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_charts, container, false);
         /**
         final TextView textView = root.findViewById(R.id.text_notifications);
@@ -36,6 +87,146 @@ public class ChartsFragment extends Fragment {
         });
          */
 
+        /**
+         * 1711326 hrl
+         * implementation of chart page
+         */
+
+        View view = root;
+
+        // init chart status
+        chartStatus = CHART_ON_UNDEFINED;
+
+        // init chart
+        lineChartView           = view.findViewById(R.id.chart_line_chart);
+        pieChartViewIncome      = view.findViewById(R.id.chart_pie_chart_income);
+        pieChartViewExpense     = view.findViewById(R.id.chart_pie_chart_expense);
+        pieChartIncomeData      = new PieChartData();
+        pieChartExpenseData     = new PieChartData();
+        lineChartData           = new LineChartData();
+        incomeLine              = new Line();
+        expenseLine             = new Line();
+        axisX                   = new Axis();
+        axisY                   = new Axis();
+
+        // init button
+        buttonSwitchToMonth     = view.findViewById(R.id.chart_toggle_button_month);
+        buttonSwitchToSeason    = view.findViewById(R.id.chart_toggle_button_season);
+        buttonSwitchToYear      = view.findViewById(R.id.chart_toggle_button_year);
+
+        // init textview
+        incomeSumDisplay        = view.findViewById(R.id.chart_income_num);
+        expenseSumDisplay       = view.findViewById(R.id.chart_expense_num);
+
+        // configure textview
+        income = 0.0;
+        expense = 0.0;
+
+        // configure chart
+        // line chart
+        final boolean lineChartHasLine = true;
+        incomeLine.setHasLines(lineChartHasLine);
+        expenseLine.setHasLines(lineChartHasLine);
+        incomeLine.setColor(LINE_CHART_INCOME_COLOR);
+        expenseLine.setColor(LINE_CHART_EXPENSE_COLOR);
+        incomeLine.setShape(LINE_CHART_INCOME_DOT);
+        expenseLine.setShape(LINE_CHART_EXPENSE_DOT);
+
+
+        // pie chart
+        final boolean pieChartHasCenterCircle         = true;
+        final boolean pieChartHasLabel                = false;
+        final boolean pieChartLabelOutside            = false;
+        final int     pieChartCenterTextSize          = 26;
+        String  pieChartCenterMessageIncome     = "收入";
+        String  pieChartCenterMessageExpense    = "支出";
+        pieChartIncomeData.setHasCenterCircle(pieChartHasCenterCircle);
+        pieChartExpenseData.setHasCenterCircle(pieChartHasCenterCircle);
+        pieChartIncomeData.setCenterText1(pieChartCenterMessageIncome);
+        pieChartExpenseData.setCenterText1(pieChartCenterMessageExpense);
+        pieChartIncomeData.setHasLabels(pieChartHasLabel);
+        pieChartExpenseData.setHasLabels(pieChartHasLabel);
+        pieChartIncomeData.setHasLabelsOutside(pieChartLabelOutside);
+        pieChartExpenseData.setHasLabelsOutside(pieChartLabelOutside);
+        pieChartIncomeData.setCenterText1FontSize(pieChartCenterTextSize);
+        pieChartExpenseData.setCenterText1FontSize(pieChartCenterTextSize);
+        // pieChartViewIncome.setPieChartData(pieChartIncomeData);
+        // pieChartViewExpense.setPieChartData(pieChartExpenseData);
+
+        // configure button
+        buttonSwitchToMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chartStatus != CHART_ON_MONTH) {
+                    switchTo(CHART_ON_MONTH);
+                }
+            }
+        });
+        buttonSwitchToSeason.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chartStatus != CHART_ON_SEASON) {
+                    switchTo(CHART_ON_SEASON);
+                }
+            }
+        });
+        buttonSwitchToYear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chartStatus != CHART_ON_YEAR) {
+                    switchTo(CHART_ON_YEAR);
+                }
+            }
+        });
+
+
+        // init data
+        switchTo(CHART_ON_MONTH);
+
         return root;
     }
+    private void switchTo(int status) {
+        buttonSwitchToMonth.setTextColor(statusInactiveColor);
+        buttonSwitchToSeason.setTextColor(statusInactiveColor);
+        buttonSwitchToYear.setTextColor(statusInactiveColor);
+        switch (status) {
+            case CHART_ON_MONTH:
+                switchToMonth();
+                buttonSwitchToMonth.setTextColor(statusActiveColor);
+                break;
+            case CHART_ON_SEASON:
+                switchToSeason();
+                buttonSwitchToSeason.setTextColor(statusActiveColor);
+                break;
+            case CHART_ON_YEAR:
+                switchToYear();
+                buttonSwitchToYear.setTextColor(statusActiveColor);
+                break;
+            case CHART_ON_UNDEFINED:
+                Log.e(" - ChartFragment", "Undefined Switch behavior occurred.");
+                break;
+        }
+        chartStatus = status;
+        incomeSumDisplay.setText(Double.toString(income));
+        expenseSumDisplay.setText(Double.toString(expense));
+
+        List<Line> lines = new ArrayList<>();
+        lines.add(incomeLine);
+        lines.add(expenseLine);
+        lineChartData.setLines(lines);
+        lineChartView.setLineChartData(lineChartData);
+    }
+
+    private void switchToMonth() {
+
+    }
+
+    private void switchToSeason() {
+
+    }
+
+    private void switchToYear() {
+
+    }
+
 }
